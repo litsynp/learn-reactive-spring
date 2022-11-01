dbab1f50c4515361c17812ae96788019f897d36c
litsynp <nocte55is@gmail.com>
2022-11-01 23:33:52 +0900

    Test mono

--- src/test/java/com/litsynp/learnreactivespring/fluxandmonoplayground/FluxAndMonoTest.java
+++ src/test/java/com/litsynp/learnreactivespring/fluxandmonoplayground/FluxAndMonoTest.java
@@ -2,6 +2,7 @@ package com.litsynp.learnreactivespring.fluxandmonoplayground;
 
 import org.junit.jupiter.api.Test;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
 
 public class FluxAndMonoTest {
@@ -73,6 +74,24 @@ public class FluxAndMonoTest {
         StepVerifier.create(stringFlux)
                 .expectNext("Spring", "Spring Boot", "Reactive Spring")
                 .expectErrorMessage("Exception occurred")
+                .verify();
+    }
+
+    @Test
+    void monoTest() {
+
+        Mono<String> stringMono = Mono.just("Spring");
+
+        StepVerifier.create(stringMono.log())
+                .expectNext("Spring")
+                .verifyComplete();
+    }
+
+    @Test
+    void monoTest_Error() {
+
+        StepVerifier.create(Mono.error(new RuntimeException("Exception occurred")).log())
+                .expectError(RuntimeException.class)
                 .verify();
     }
 }
