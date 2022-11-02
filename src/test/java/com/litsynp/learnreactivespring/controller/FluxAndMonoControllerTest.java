fd3d363a95bc0e06cb7967958b534cd5f5802525
litsynp <nocte55is@gmail.com>
2022-11-03 00:53:30 +0900

    Add mono API

--- src/test/java/com/litsynp/learnreactivespring/controller/FluxAndMonoControllerTest.java
+++ src/test/java/com/litsynp/learnreactivespring/controller/FluxAndMonoControllerTest.java
@@ -103,4 +103,19 @@ class FluxAndMonoControllerTest {
                 .thenCancel()
                 .verify();
     }
+
+    @Test
+    void mono() {
+
+        Integer expectedValue = 1;
+
+        webTestClient.get().uri("/mono")
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody(Integer.class)
+                .consumeWith((response) -> {
+                    assertThat(response.getResponseBody()).isEqualTo(expectedValue);
+                });
+    }
 }
